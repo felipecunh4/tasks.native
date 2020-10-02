104609dbbbd2c3c857be80e06ee3678179ef07fd
Felipe Cunha <felipe.cunha@afya.com.br>
2020-10-02 15:44:05 -0300

    logout

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -11,7 +11,17 @@ import {DrawerItems} from 'react-navigation-drawer';
 import {Gravatar} from 'react-native-gravatar';
 import commonStyles from '../commonStyles';
 
+import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 export default props => {
+  const logout = () => {
+    delete axios.defaults.headers.common['Authorization'];
+    AsyncStorage.removeItem('userData');
+    props.navigation.navigate('AuthOrApp');
+  };
+
   return (
     <ScrollView>
       <View style={styles.header}>
@@ -27,6 +37,11 @@ export default props => {
           <Text style={styles.name}>{props.navigation.getParam('name')}</Text>
           <Text style={styles.email}>{props.navigation.getParam('email')}</Text>
         </View>
+        <TouchableOpacity onPress={logout}>
+          <View style={styles.logoutIcon}>
+            <Icon name="sign-out" size={30} color="#800" />
+          </View>
+        </TouchableOpacity>
       </View>
       <DrawerItems {...props} />
     </ScrollView>
